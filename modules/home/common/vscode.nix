7eb72aa4c02075f467db18c446a93d203e7210af
mimi <mimi@bnuuy.garden>
2026-09-16 13:00:39 +0200

    feat: add steam

--- modules/home/common/vscode.nix
+++ modules/home/common/vscode.nix
@@ -13,7 +13,7 @@
         bbenoist.nix
         fill-labs.dependi
         bradlc.vscode-tailwindcss
-        brettm12345.nixfmt-vscode
+        # brettm12345.nixfmt-vscode
         # cordx56.rustowl-vscode - isnt there yet i think  
       ];
 
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -13,40 +13,50 @@
     };
   };
 
-  programs.noctalia-greeter = {
-    enable = true;
-    greeter-args = "";
-    settings = {
-      cursor = {
-        theme = "Bibata-Modern-Ice";
-        size = 24;
-        path = "${pkgs.bibata-cursors}/share/icons";
-      };
-      keyboard = {
-        layout = "de";
+  programs = {
+    noctalia-greeter = {
+      enable = true;
+      greeter-args = "";
+      settings = {
+        cursor = {
+          theme = "Bibata-Modern-Ice";
+          size = 24;
+          path = "${pkgs.bibata-cursors}/share/icons";
+        };
+        keyboard = {
+          layout = "de";
+        };
       };
     };
-  };
 
-  programs.ioquake3 = {
-    enable = true;
-    settings = {
-      sensitivity = 0.6;
-      name = "mimi";
-      com_maxfps = 280;
-      cg_drawFPS = true;
-      cg_fov = 103;
-      r_mode = "-1";
-      r_customheight = 1440;
-      r_customwidth = 2560;
+   ioquake3 = {
+      baseq3 = "/home/mimi/.local/state/Quake3";
+      enable = true;
+      settings = {
+        sensitivity = 0.6;
+        name = "mimi";
+        com_maxfps = 280;
+        cg_drawFPS = true;
+        cg_fov = 103;
+        r_mode = "-1";
+        r_customheight = 1440;
+        r_customwidth = 2560;
+      };
     };
-  };
 
-  programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      droidcam-obs
-    ];
+    obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        droidcam-obs
+      ];
+    };
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
   };
 }
